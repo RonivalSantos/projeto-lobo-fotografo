b294821acc7b7f3349b04115ba4471fed80ba38c
RonivalSantos <ronivalsantos16@gmail.com>
2025-03-16 19:13:10 -0300

    commit section imgs

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ mobileBtn.onclick = () => {
   mobileMenu.classList.toggle("ativo");
   mobileBtn.classList.toggle("trocar");
 };
+
+//scroll suave
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+linksInternos.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});

46e065c3bf8770150b300f1fcda4f1acfca91752
RonivalSantos <ronivalsantos16@gmail.com>
2025-04-17 14:21:14 -0300

    alteracoes no menu mobile

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
+// const mobileBtn = document.querySelector("#mobile-btn");
+// const mobileMenu = document.querySelector("#mobile-menu");
+// mobileBtn.onclick = () => {
+//   mobileMenu.classList.toggle("ativo");
+//   mobileBtn.classList.toggle("trocar");
+// };
+
+
 const mobileBtn = document.querySelector("#mobile-btn");
-const mobileMenu = document.querySelector("#mobile-menu");
-mobileBtn.onclick = () => {
-  mobileMenu.classList.toggle("ativo");
-  mobileBtn.classList.toggle("trocar");
-};
+  const mobileMenu = document.querySelector("#mobile-menu");
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
+
+  // Função para fechar menu
+  function fecharMenu() {
+    mobileMenu.classList.remove("ativo");
+    mobileBtn.classList.remove("trocar");
+    overlay.classList.remove("ativo");
+  }
+
+  mobileBtn.onclick = () => {
+    mobileMenu.classList.toggle("ativo");
+    mobileBtn.classList.toggle("trocar");
+    overlay.classList.toggle("ativo");
+  };
+
+  // Fecha o menu ao clicar fora
+  overlay.onclick = fecharMenu;
+
+  // Fecha o menu ao clicar em qualquer link dentro dele
+  const linksMenu = mobileMenu.querySelectorAll("a");
+  linksMenu.forEach((link) => {
+    link.addEventListener("click", fecharMenu);
+  });
 
 //scroll suave
 const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
